d2390588bc8b2ae44eb0242a5e04b5b7bf0fcb2f
Sudipto Ghosh <sudipto@jhatkaa.org>
2023-10-04 19:10:03 +0530

    fixes the doc issue

--- tests/flytekit/unit/core/test_map_task.py
+++ tests/flytekit/unit/core/test_map_task.py
@@ -53,7 +53,7 @@ def test_map_docs():
     def my_wf(x: typing.List[int]) -> typing.List[typing.Optional[str]]:
         return map_task(my_mappable_task, metadata=TaskMetadata(retries=1), concurrency=10, min_success_ratio=0.75,)(
             a=x
-        ).with_overrides(cpu="10M")
+        ).with_overrides(dsadsadsa)
 
     # test_map_task_end
 
